ecaaeb9920e99abd0333bb723bfda7c31f7acaa4
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2023-05-19 10:58:52 +0200

    fix: Separated processing from API

--- succulent/api.py
+++ succulent/api.py
@@ -1,61 +1,31 @@
-import os
-import pandas as pd
 from flask import Flask, jsonify, request
 from succulent.configuration import Configuration
+from succulent.processing import Processing
 
-conf = Configuration('configuration.yml');
 # Configuration file
+conf = Configuration('configuration.yml');
 config = conf.load_config()
 
-# Data configuration
-columns = [configuration['name'] for configuration in config['data']]
-filetype = config['filetype']
+# Initialise processing
+processing = Processing(config)
 
+# Initialise Flask
 app = Flask(__name__)
 
 @app.route('/measure', methods=['GET'])
 def url():
-    parameters = [f'{column}=' for column in columns]
-    params = '&'.join(parameters)
-    return jsonify({'url': f'0.0.0.0:8080/measure?{params}' }), 200
+    # Generate URL
+    parameters = processing.parameters()
+
+    # Send response
+    return jsonify({'url': f'0.0.0.0:8080/measure?{parameters}' }), 200
 
 @app.route('/measure', methods=['POST'])
 def measure():
-    # Define paths
-    path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'data', f'data.{filetype}'
-    )
-    output_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'data', f'data.{filetype}'
-    )
+    # Process request
+    processing.process(request)
 
-    # Load existing data
-    if os.path.exists(path):
-        if filetype == 'csv':
-            df = pd.read_csv(path, sep=',')
-        elif filetype == 'json':
-            df = pd.read_json(path, orient='records')
-    # Initialise new data
-    else:
-        df = pd.DataFrame(columns=columns)
-
-    # Parse data from request
-    if request.is_json:
-        data = [request.json[column] for column in columns]
-    else:
-        data = [request.args.get(column) for column in columns]
-    new_data = pd.Series(data, index=columns)
-
-    # Merge data
-    df = pd.concat([df, new_data.to_frame().T], ignore_index=True)
-
-    # Store data to device
-    match filetype:
-        case 'csv':
-            df.to_csv(output_path, sep=',', index=False)
-        case 'json':
-            df.to_json(output_path, orient='records', indent=4)
-
-    return jsonify({'success': True}), 200
+    # Send response
+    return jsonify({'message': 'Data stored'}), 200
 
 app.run(host='0.0.0.0', port=8080)
---
+++ succulent/processing.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+
+class Processing:
+    def __init__(self, config):
+        self.filetype = config['filetype']
+        self.columns = [configuration['name'] for configuration in config['data']]
+    
+    def parameters(self):
+        parameters = [f'{column}=' for column in self.columns]
+        parameters = '&'.join(parameters)
+        return parameters
+        
+    def process(self, req):
+        # Define paths
+        path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'data', f'data.{self.filetype}'
+        )
+        output_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'data', f'data.{self.filetype}'
+        )
+
+        # Load existing data
+        if os.path.exists(path):
+            if self.filetype == 'csv':
+                df = pd.read_csv(path, sep=',')
+            elif self.filetype == 'json':
+                df = pd.read_json(path, orient='records')
+        # Initialise new data
+        else:
+            df = pd.DataFrame(columns=self.columns)
+
+        # Parse data from request
+        if req.is_json:
+            data = [req.json[column] for column in self.columns]
+        else:
+            data = [req.args.get(column) for column in self.columns]
+        new_data = pd.Series(data, index=self.columns)
+
+        # Merge data
+        df = pd.concat([df, new_data.to_frame().T], ignore_index=True)
+
+        # Store data to device
+        match self.filetype:
+            case 'csv':
+                df.to_csv(output_path, sep=',', index=False)
+            case 'json':
+                df.to_json(output_path, orient='records', indent=4)
