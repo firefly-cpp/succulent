c8dcc3e126e4a8e79d98f5dc3e310d36945faec5
firefly-cpp <iztok@iztok-jr-fister.eu>
2023-06-11 12:27:53 +0200

    edit docs config file

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
@@ -24,14 +24,12 @@ author = 'Tadej Lahovnik, Iztok Fister Jr.'
 # The full version, including alpha/beta/rc tags
 release = '0.2.3'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinxcontrib.bibtex']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,9 +45,22 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Bibtex
+bibtex_bibfiles = ['refs.bib']
+
+# Bibliography style
+bibtex_default_style = 'unsrt'
+
+# Add logo for project
+html_logo = '../.github/images/logo.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
