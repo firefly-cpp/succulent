9fa5bfd4040a63850191e85eb330aa6d0b202aa1
firefly-cpp <iztok@iztok-jr-fister.eu>
2023-07-08 11:08:49 +0200

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Tadej Lahovnik, Iztok Fister Jr.'
 author = 'Tadej Lahovnik, Iztok Fister Jr.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 # -- General configuration ---------------------------------------------------
 
--- succulent/__init__.py
+++ succulent/__init__.py
@@ -3,4 +3,4 @@ from succulent.configuration import Configuration
 __all__ = [
     'Configuration']
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
