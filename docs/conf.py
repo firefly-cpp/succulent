28a2c20e2ebeefd0b2098a1761614471570f703c
Iztok Fister Jr. <iztok@iztok-jr-fister.eu>
2023-12-08 17:25:55 +0100

    Bump version to 0.2.7

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Tadej Lahovnik, Iztok Fister Jr.'
 author = 'Tadej Lahovnik, Iztok Fister Jr.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.6'
+release = '0.2.7'
 
 # -- General configuration ---------------------------------------------------
 
